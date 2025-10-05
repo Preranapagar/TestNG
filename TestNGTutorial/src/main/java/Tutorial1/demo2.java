1107fffc1b524da94dde114cda97255f528e901c
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-10-05 14:06:29 +0530

    demo2

--- TestNGTutorial/src/main/java/Tutorial1/demo2.java
+++ TestNGTutorial/src/main/java/Tutorial1/demo2.java
@@ -1,5 +1,25 @@
 package Tutorial1;
 
+import org.testng.annotations.Test;
+
 public class demo2 {
+	
+	@Test(priority=1)
+	public void logo()
+	{
+		System.out.println("logo method");
+	}
+	
+	@Test(priority=2)
+	public void loginMethod()
+	{
+		System.out.println("login method");
+	}
+	
+	@Test(priority=3)
+	public void alpha()
+	{
+		System.out.println("alpha methoda");
+	}
 
 }
