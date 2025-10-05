24415e926c330317887eb98c75a23ac3691bd09b
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-10-05 13:51:22 +0530

    update

---
+++ TestNGTutorial/src/main/java/Tutorial1/demo1.java
@@ -0,0 +1,25 @@
+package Tutorial1;
+
+import org.testng.annotations.Test;
+
+public class demo1 {
+	
+	@Test
+	public void logValidate()
+	{
+		System.out.println("logo validate method");
+	}
+	
+	@Test
+	public void login()
+	{
+		System.out.println("login method");
+	}
+	
+	@Test
+	public void homeTitle()
+	{
+		System.out.println("Home Title method");
+	}
+
+}
