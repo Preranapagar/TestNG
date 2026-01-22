5c3d556bfad94baf48ca24a7041e3a99b50be42f
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2026-01-22 10:43:21 +0530

    update

--- TestNGTutorial/src/main/java/Tutorial1/demo4.java
+++ TestNGTutorial/src/main/java/Tutorial1/demo4.java
@@ -1,5 +1,29 @@
 package Tutorial1;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class demo4 {
+	
+	public static ChromeDriver driver;
+	
+	@Test(priority = 1)
+	public void login()
+	{
+		driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[text()=' Login ']")).click();
+	}
+	
+	
 
 }
